6c83ee171d8fefafe18a7c28b84349a812b44a6f
Stephano Wurttele <stephano@utec.edu.pe>
2021-07-09 20:00:20 -0500

    Tests

--- ejercicio3b.cpp
+++ ejercicio3b.cpp
@@ -7,12 +7,12 @@
 #include <stdlib.h>     /* srand, rand */
 #include <time.h>
 #include <chrono>
-#define N 320
+#define N 960
 using namespace std;
 
 void MatVecMult(double matrix[N][N], double v[N],int threads){
     double result[N]={0};
-    #pragma omp parallel for  num_threads(threads)
+    #pragma omp parallel for num_threads(threads) schedule(static)
     for (int i=0;i<N;i++){
         //printf("la iteracion i:%d la hizo el proceso: %d \n",i,omp_get_thread_num());
         for (int j=0;j<N;j++){
@@ -33,17 +33,17 @@ int main(){
     std::chrono::steady_clock::time_point begin = std::chrono::steady_clock::now();
     MatVecMult(matrix,v,2);
     std::chrono::steady_clock::time_point end = std::chrono::steady_clock::now();
-    cout<<"time was with 2 threads: "<<std::chrono::duration_cast<std::chrono::microseconds>(end - begin).count()<<"microsegundos"<<endl;
+    cout<<"time was with 2 threads: "<<std::chrono::duration_cast<std::chrono::microseconds>(end - begin).count()<<" microsegundos"<<endl;
     begin = std::chrono::steady_clock::now();
     MatVecMult(matrix,v,4);
     end = std::chrono::steady_clock::now();
-    cout<<"time was with 4 threads: "<<std::chrono::duration_cast<std::chrono::microseconds>(end - begin).count()<<"microsegundos"<<endl;
+    cout<<"time was with 4 threads: "<<std::chrono::duration_cast<std::chrono::microseconds>(end - begin).count()<<" microsegundos"<<endl;
     begin = std::chrono::steady_clock::now();
     MatVecMult(matrix,v,8);
     end = std::chrono::steady_clock::now();
-    cout<<"time was with 8 threads: "<<std::chrono::duration_cast<std::chrono::microseconds>(end - begin).count()<<"microsegundos"<<endl;
+    cout<<"time was with 8 threads: "<<std::chrono::duration_cast<std::chrono::microseconds>(end - begin).count()<<" microsegundos"<<endl;
     begin = std::chrono::steady_clock::now();
     MatVecMult(matrix,v,16);
     end = std::chrono::steady_clock::now();
-    cout<<"time was with 16 threads: "<<std::chrono::duration_cast<std::chrono::microseconds>(end - begin).count()<<"microsegundos"<<endl;
+    cout<<"time was with 16 threads: "<<std::chrono::duration_cast<std::chrono::microseconds>(end - begin).count()<<" microsegundos"<<endl;
 }
--- ejercicio3c.cpp
+++ ejercicio3c.cpp
@@ -3,7 +3,7 @@
 #include <stdlib.h>
 #include <time.h>       /* time */
 using namespace std;
-#define N 120
+#define N 960
 
 int main(int argc, char *argv[]) {
     int me, numprocs;
